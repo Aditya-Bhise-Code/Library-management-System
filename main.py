46b0c278d3f205d9ed74f775887fa317265ccc7a
Aditya Bhise <adityabhise0007@gmail.com>
2026-09-02 23:06:08 +0530

    added view book and search book but small error in search book

--- main.py
+++ main.py
@@ -21,12 +21,15 @@ def menu():
     print("6.Exit")
     print()
 
-
+book_data = [] #initialization
 def read_book():
-    with open("book_data.txt","r") as f:
-        book_data = f.readlines()
-    return book_data
-
+    try:
+        with open("book_data.txt","r") as f:
+            book_data = f.readlines()
+        return book_data
+    except FileNotFoundError:
+        book_data = []
+        return book_data
 
 def addbook():
     #Dockstring
@@ -59,27 +62,29 @@ def addbook():
 def view_book():
     book_data = read_book()
     print("Sr.no , Book_Name , Book_Author , Book_Quantity")
-    for i in book_data:
-        print(Fore.GREEN + i)
-        print()
+    if len(book_data) == 0:
+        print(Fore.RED + "There are no book in Database to view")
+    else:
+        for i in book_data:
+            i = i.replace("\n","")
+            i = i.split(",")
+            print(Fore.LIGHTYELLOW_EX + f"{i[0]} - {i[1]} - {i[-1]}")
 
-
-
-
-
-
-
-def search_book():     #Doubt
+def search_book(param):
     print(Fore.GREEN + "---Search Book---")
-    book_name = input("Enter book name  :").title()
     book_data = read_book()
-    for i in book_data:
-        if book_name in i:
-            print(Fore.GREEN + "Yes we have that book")
-            break
-        else:
-            print(Fore.LIGHTRED_EX + "Sorry we don't have the book")
-
+    if len(book_data) == 0:
+            print(Fore.RED + "There are no book in Database to Search")
+    else:
+        for i in book_data:
+            i = i.replace("\n","")
+            i = i.split(",")
+            if param.isdigit():
+                if i[0] == param + " ":
+                    print("\n\n")
+                    print(Fore.LIGHTYELLOW_EX + f"{i[0]} - {i[1]} - {i[-1]}")
+            else:
+                print("ok")
 
 
 
@@ -95,7 +100,8 @@ while True:
     elif choice == "2":
         view_book()
     elif choice == "3":
-        print("Search Book")
+        val = input("Enter the book Id  :")
+        search_book(val)
     elif choice == "4":
         print("Register Book")
     elif choice == "5":
